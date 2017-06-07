7161c54e082e7248b88d98470d3192a61e8c4a6e
yuusuke <yuusukeogawata@gmail.com>
2017-06-07 14:16:26 +0900

    設定画面のレイアウトを追加

---
+++ app/src/main/java/com/example/yuusuke/myrobotcontroller/robotSettingActivity.java
@@ -0,0 +1,14 @@
+package com.example.yuusuke.myrobotcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class robotSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robot_setting);
+    }
+
+}
